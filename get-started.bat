6d803204c74333aff62cdf8bee8518026f3983fa
Jyri Ilama <jyri.ilama@kone.com>
2020-11-05 10:34:59 +0200

    Improve Windows startup script echos

--- get-started.bat
+++ get-started.bat
@@ -16,7 +16,8 @@ echo.
 echo Starting jenking docker image...
 docker run -p 8080:8080 --env JAVA_OPTS=-Dhudson.model.DirectoryBrowserSupport.CSP= -v jenkins_home://var/jenkins_home -v //var/run/docker.sock:/var/run/docker.sock --detach --name robodemo robodemo || goto :ERROR
 echo.
-echo Docker is now running open your browser: http://localhost:8080
+echo Jenkins is now running!
+echo Open your browser to: http://localhost:8080
 echo Login with admin / RoboCorpRules2020!
 
 
